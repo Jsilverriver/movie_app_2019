2af80b726cfef3ed1a259f08805c8a4cf0ca5d3c
EugangJi <jeggenius@gmail.com>
2019-11-24 22:06:32 +0900

    #2.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food1(asdf) {
+  // properties를 argument(인자)로 넣음. props라는 이름으로 대신해줌. 이름은은 아무거나 됨.
+  console.log(asdf, asdf.fav, asdf["fav"]);
+  return <p>Food1 see the console</p>;
+}
+
+function Food2({ fav }) {
+  return <h3>I love {fav}</h3>;
+}
 
 function App() {
   return (
     <div>
-      <h1></h1>Hello!
-      <Potato />
+      <h1>Hello!</h1>
+      <Food1 fav="veges" />
+      <Food2 fav="samgyeopsal" />
+      <Food2 fav="abocado" />
+      <Food2 fav="ichigo" />
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// Potato 이렇게 첫문자를 대문자로 넣어야 한다!
-function Potato() {
-  return <h3> I love react! </h3>;
-}
-
-export default Potato;
-//왜 하는진 모르겠지만 해야만함.
