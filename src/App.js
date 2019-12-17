a547fd8cce169abf5e7793c5a42f639c88e104a5
Eungang Ji <jeggenius@gmail.com>
2019-12-17 20:55:18 +0900

    #3.3

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movie: []
   };
 
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-
-  minust = () => {};
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false, book: false });
+    }, 6000);
+  }
 
   render() {
-    return (
-      <div>
-        <h1>The counted Number is : {this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready!"}</div>;
   }
 }
 
